095898797350bc8a378d68b3e4699327c66ead79
Sai-778 <sai423453@gmail.com>
2025-03-10 17:59:53 +0530

    final commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ class DrumKit {
     this.isPlaying = null;
     this.selects = document.querySelectorAll("select");
     this.muteBtns = document.querySelectorAll(".mute");
-    this.tempoSlider=document.querySelector(".tempo-slider");
+    this.tempoSlider = document.querySelector(".tempo-slider");
   }
 
   activePad() {
@@ -90,48 +90,47 @@ class DrumKit {
   mute(e) {
     const muteIndex = e.target.getAttribute("data-track");
     e.target.classList.toggle("active");
-    if( e.target.classList.contains("active")){
-      switch(muteIndex){
+    if (e.target.classList.contains("active")) {
+      switch (muteIndex) {
         case "0":
-        this.kickAudio.volume = 0;
-        break;
+          this.kickAudio.volume = 0;
+          break;
         case "1":
           this.snareAudio.volume = 0;
           break;
-          case "2":
-            this.hihatAudio.volume = 0;
-            break;
+        case "2":
+          this.hihatAudio.volume = 0;
+          break;
       }
-    }else{
-      switch(muteIndex){
+    } else {
+      switch (muteIndex) {
         case "0":
-        this.kickAudio.volume = 1;
-        break;
+          this.kickAudio.volume = 1;
+          break;
         case "1":
           this.snareAudio.volume = 1;
           break;
-          case "2":
-            this.hihatAudio.volume = 1;
-            break;
+        case "2":
+          this.hihatAudio.volume = 1;
+          break;
       }
     }
   }
 
-  changeTempo(e){
-    const tempoTxt= document.querySelector(".tempo-nr");
-    tempoTxt.innerText=e.target.value;
+  changeTempo(e) {
+    const tempoTxt = document.querySelector(".tempo-nr");
+    tempoTxt.innerText = e.target.value;
   }
 
-  updateTempo(e){
-    this.bpm=e.target.value;
+  updateTempo(e) {
+    this.bpm = e.target.value;
     clearInterval(this.isPlaying);
-    this.isPlaying=null;
-    const playBtn =document.querySelector(".play");
-    if(playBtn.classList.contains("active")){
-          this.start();
+    this.isPlaying = null;
+    const playBtn = document.querySelector(".play");
+    if (playBtn.classList.contains("active")) {
+      this.start();
     }
   }
-
 }
 
 const drumKit = new DrumKit();
@@ -162,12 +161,10 @@ drumKit.muteBtns.forEach((btn) => {
   });
 });
 
-drumKit.tempoSlider.addEventListener('input',function(e){
+drumKit.tempoSlider.addEventListener("input", function (e) {
   drumKit.changeTempo(e);
+});
 
-})
-
-drumKit.tempoSlider.addEventListener('change',function(e){
+drumKit.tempoSlider.addEventListener("change", function (e) {
   drumKit.updateTempo(e);
-
-})
+});
